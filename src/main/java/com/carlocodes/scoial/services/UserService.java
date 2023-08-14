95bda0d5ae0588f0858a34fce1d60d8d4be73cb4
carlocodes <Gian Uy>
2023-08-14 20:47:26 +0800

    verify endpoint

--- src/main/java/com/carlocodes/scoial/services/UserService.java
+++ src/main/java/com/carlocodes/scoial/services/UserService.java
@@ -5,9 +5,11 @@ import com.carlocodes.scoial.dtos.UserDto;
 import com.carlocodes.scoial.entities.User;
 import com.carlocodes.scoial.exceptions.SocialException;
 import com.carlocodes.scoial.repositories.UserRepository;
+import com.carlocodes.scoial.utilities.InMemoryOtpCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -40,6 +42,9 @@ public class UserService {
                 userRepository.save(user);
 
                 Otp otp = otpService.generateOtp();
+                // TODO: Add logic to check if there is an existing otp in place already
+                // If so, remove the existing OTP first and then add the new one
+                InMemoryOtpCache.put(email, otp);
 
                 emailService.sendOtp(email, otp);
             } else {
@@ -47,6 +52,39 @@ public class UserService {
             }
         } catch (SocialException e) {
             throw new SocialException(String.format("Register failed for email: %s due to %s", userDto.getEmail(), e.getMessage()), e);
+        }
+    }
+
+    public void verify(UserDto userDto) throws SocialException {
+        try {
+            String email = userDto.getEmail();
+            String otp = userDto.getOtp();
+
+            Optional<User> optionalUser = userRepository.findByEmail(email);
+
+            if (optionalUser.isPresent()) {
+                Otp storedOtp = InMemoryOtpCache.get(email);
+                LocalDateTime now = LocalDateTime.now();
+                String storedOtpValue = storedOtp.getValue();
+                LocalDateTime storedOtpExpiration = storedOtp.getExpiration();
+
+                if (!otp.equals(storedOtpValue))
+                    throw new SocialException("OTP does not match!");
+                // TODO: Use constant value for minutes
+                if (now.minusMinutes(10).isAfter(storedOtpExpiration))
+                    throw new SocialException("OTP expired!");
+
+                User user = optionalUser.get();
+                user.setVerified(true);
+                userRepository.save(user);
+
+                // TODO: Authenticate and authorize
+                // Return jwt
+            } else {
+                throw new SocialException(String.format("User not found for email: %s", email));
+            }
+        } catch (SocialException e) {
+            throw new SocialException(String.format("Verify failed for email: %s due to %s", userDto.getEmail(), e.getMessage()), e);
         }
     }
 }

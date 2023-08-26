165e1a9fdf4376ab50f2b2fcf02193630fe53268
carlocodes <Gian Uy>
2023-08-26 21:20:37 +0800

    entity changes, post classes, exception message enhancements

---
+++ src/main/java/com/carlocodes/scoial/controllers/PostController.java
@@ -0,0 +1,27 @@
+package com.carlocodes.scoial.controllers;
+
+import com.carlocodes.scoial.dtos.PostDto;
+import com.carlocodes.scoial.exceptions.SocialException;
+import com.carlocodes.scoial.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/post")
+public class PostController {
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<PostDto> create(@RequestBody PostDto postDto) throws SocialException {
+        return ResponseEntity.ok(postService.create(postDto));
+    }
+}
---
+++ src/main/java/com/carlocodes/scoial/services/PostService.java
@@ -0,0 +1,52 @@
+package com.carlocodes.scoial.services;
+
+import com.carlocodes.scoial.dtos.PostDto;
+import com.carlocodes.scoial.entities.Post;
+import com.carlocodes.scoial.entities.User;
+import com.carlocodes.scoial.exceptions.SocialException;
+import com.carlocodes.scoial.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    private final PostRepository postRepository;
+    private final UserService userService;
+
+    @Autowired
+    public PostService(PostRepository postRepository,
+                       UserService userService) {
+        this.postRepository = postRepository;
+        this.userService = userService;
+    }
+
+    public PostDto create(PostDto postDto) throws SocialException {
+        try {
+            Long userId = postDto.getUserId();
+
+            User user = userService.findById(userId)
+                    .orElseThrow(() -> new SocialException(String.format("User with id: %s does not exist!", userId)));
+
+            return mapToDto(save(postDto, user));
+        } catch (SocialException e) {
+            throw new SocialException(String.format("Create post failed for user with id: %s due to %s", postDto.getUserId(), e.getMessage()), e);
+        }
+    }
+
+    private Post save(PostDto postDto, User user) {
+        Post post = new Post();
+        post.setImage(postDto.getImage());
+        post.setMessage(postDto.getMessage());
+        post.setUser(user);
+        return postRepository.save(post);
+    }
+
+    private PostDto mapToDto(Post post) {
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setImage(post.getImage());
+        postDto.setMessage(post.getMessage());
+        postDto.setUserId(post.getUser().getId());
+        return postDto;
+    }
+}

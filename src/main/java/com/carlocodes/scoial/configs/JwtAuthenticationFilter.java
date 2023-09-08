b65d2abf4a4691aa13bf4cbdf3d100b7376786c2
carlocodes <Gian Uy>
2023-09-08 23:59:55 +0800

    spacing

--- src/main/java/com/carlocodes/scoial/configs/JwtAuthenticationFilter.java
+++ src/main/java/com/carlocodes/scoial/configs/JwtAuthenticationFilter.java
@@ -51,6 +51,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
         }
+
         filterChain.doFilter(request, response);
     }
 }
